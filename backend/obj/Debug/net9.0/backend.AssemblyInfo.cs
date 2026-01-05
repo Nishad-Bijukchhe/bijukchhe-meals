3184c76cf8582bf0e19475e6a10d3d92f48338fd
Nishad-Bijukchhe <nishad.bijukchhe1@gmail.com>
2026-01-05 10:59:39 +0545

    setup backend

--- backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net9.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36e2a7d28b8e60f50d5d76de9e7e5816095fd9fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b788658046ad1fa8c882a14204bfca46c74a1cc1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
